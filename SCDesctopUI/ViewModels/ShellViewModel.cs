ff4eb6ceb028d4555ca31f358e3c5e86f2d4b452
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-06-22 20:26:53 +0200

    Add Simple Calculator Layout and Basic Buttons Functionality.
    Creates simple calculator beeing able only to display pressed button.

--- SCDesctopUI/ViewModels/ShellViewModel.cs
+++ SCDesctopUI/ViewModels/ShellViewModel.cs
@@ -1,12 +1,118 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SCDesctopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Screen
     {
-    }
+		private string _calculatorDisplay;
+
+		public string CalculatorDisplay
+		{
+			get { return _calculatorDisplay; }
+			set { _calculatorDisplay = value; NotifyOfPropertyChange(() => CalculatorDisplay); }
+		}
+
+		public bool CanMakeNumericInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeNumericInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeNumericInput);
+		}
+
+		public bool CanMakeDotInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeDotInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeDotInput);
+		}
+
+		public bool CanMakeEqualInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeEqualInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeEqualInput);
+		}
+
+		public bool CanMakeBinaryOperatorInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeBinaryOperatorInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeBinaryOperatorInput);
+		}
+
+		public bool CanMakeParanthesesInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeParanthesesInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeParanthesesInput);
+		}
+
+		public bool CanMakeEraseInput
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(CalculatorDisplay))
+					return true;
+				return true;
+			}
+		}
+
+		public void MakeEraseInput(string text)
+		{
+			CalculatorDisplay += text;
+			NotifyOfPropertyChange(() => CanMakeEraseInput);
+		}
+
+	}
 }
